9292831281eb361e1d4c64e710c17f93fc2dac3e
Vincent Bernat <Vincent.Bernat@exoscale.ch>
2017-07-07 16:53:27 +0200

    Use old hash style for compatibility with Ruby 1.8

--- lib/puppet/provider/cumulus_license/cumulus.rb
+++ lib/puppet/provider/cumulus_license/cumulus.rb
@@ -2,14 +2,14 @@ Puppet::Type.type(:cumulus_license).provide :cumulus do
   # If cl_license is missing or not configured as an exec
   # Puppet returns error:
   # "Could not find a suitable provider for cumulus_license"
-  commands cl_license: '/usr/cumulus/bin/cl-license'
+  commands :cl_license => '/usr/cumulus/bin/cl-license'
 
   # If operating system is not cumulus
   # Puppet returns error:
   # "Could not find a suitable provider for cumulus_license"
   # someone asked for a way to log why provider is not suitable
   # but unable to find progress on it. http://bit.ly/17Dhny6
-  confine operatingsystem: [:cumuluslinux]
+  confine :operatingsystem => [:cumuluslinux]
 
   def exists?
     # if license is forced to be installed, install it.
